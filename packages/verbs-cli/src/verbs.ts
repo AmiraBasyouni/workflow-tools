3842cddbb0e5657b30c4c278aa2b24f22c15680f
Amira Basyouni <work.amirab@gmail.com>
2026-09-05 08:29:44 -0700

    feat(verbs): add verb 'branch create'.
    Signed-off-by: Amira Basyouni <work.amirab@gmail.com>

--- packages/verbs-cli/src/verbs.ts
+++ packages/verbs-cli/src/verbs.ts
@@ -20,7 +20,56 @@ const verbs: Segment = {
             type: "process",
             program: "git",
             args: ["branch", "--color=always"],
-            options: { stdout: true },
+            options: { stdout: "inherit" },
+          },
+        ],
+      },
+    },
+    create: {
+      workflow: {
+        name: "branch create",
+        requirements: [
+          {
+            type: "program",
+            description: "git is installed",
+            verificationSteps: [
+              { type: "process", program: "git", args: ["--version"] },
+            ],
+            fulfillmentInstructions: ["sudo apt install -g git"],
+          },
+          {
+            type: "program",
+            description: "@amirab/prompt is installed",
+            verificationSteps: [
+              {
+                type: "process",
+                program: "prompt",
+                args: ["--version"],
+                options: { stdout: "pipe" },
+              },
+              {
+                type: "process",
+                program: "grep",
+                args: ["^@amirab/prompt"],
+                options: { stdin: "pipe" },
+              },
+            ],
+            fulfillmentInstructions: ["npm install -g @amirab/prompt"],
+          },
+        ],
+        steps: [
+          {
+            type: "process",
+            program: "prompt",
+            args: ["New branch name: ", "--type", "string", "--null"],
+            // stdin from terminal, stderr to terminal, stdout to pipe
+            options: { stdin: "inherit", stdout: "pipe", stderr: "inherit" },
+          },
+          {
+            type: "process",
+            program: "xargs",
+            args: ["-0", "git", "branch"],
+            options: { stdin: "pipe", stdout: "inherit" },
           },
         ],
       },
